59e732e0e68230a61f015c274bf12c9b3102a7c1
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2016-10-24 14:59:22 +0530

    feat(VecVec::new)

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,24 @@ pub struct VecVec<T> {
     height: usize,
 }
 
+impl<T> VecVec<T> {
+    pub fn new(width: usize, height: usize, value: T) -> Self
+        where T: Clone
+    {
+        VecVec {
+            inner: vec![value; width * height],
+            width: width,
+            height: height,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {}
+    fn it_works() {
+        let _vv = VecVec::new(4, 3, 'a');
+    }
 }
